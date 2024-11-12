f2ebd659d7c65953ff7bf59c997fdd34cb3f616e
Paung <paungcuy15@gmail.com>
2024-11-12 09:26:46 +0700

    side bar update

--- components/Post/index.vue
+++ components/Post/index.vue
@@ -265,9 +265,9 @@
               <div>
                 <img class="h-auto max-w-full " src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-3.jpg" alt="">
               </div>
-              <!-- <div>
+              <div>
                 <img class="h-auto max-w-full " src="https://flowbite.s3.amazonaws.com/docs/gallery/square/image-4.jpg" alt="">
-              </div> -->
+              </div>
             </div>
           </div>
 
