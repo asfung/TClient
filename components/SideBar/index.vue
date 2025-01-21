1d0d94432627273d1644502bd8a6fd79704d9aa4
Paung <paungcuy15@gmail.com>
2025-01-21 11:54:27 +0700

    sidebar and some routing config

--- components/SideBar/index.vue
+++ components/SideBar/index.vue
@@ -1,22 +1,18 @@
 <template>
-  <!-- <div class="profile-section"> -->
   <div class="h-96 dark:bg-black dark:text-white p-7 rounded-lg dark:border dark:border-white">
-    <!-- <h1>Side Bar</h1>
-    sidebar selesai -->
-    <!-- Profile -->
-    <!-- <v-card class="mx-auto" max-width="300"> -->
     <v-list class="dark:text-white dark:bg-black -mx-5">
-      <!-- <v-list-subheader>REPORTS</v-list-subheader> -->
-
-      <v-list-item v-for="(item, i) in items" :key="i" :value="item">
+      <v-list-item v-for="(item, i) in items" :key="i" :value="item" :active="item.name === $route.name" class="sidebar-item hover:bg-blue-400">
         <template v-slot:prepend>
           <v-icon :icon="item.icon"></v-icon>
         </template>
-
         <v-list-item-title v-text="item.text"></v-list-item-title>
       </v-list-item>
     </v-list>
-    <!-- </v-card> -->
+    <!-- <ul v-for="(item, i) in items" :key="i" :value="item"> -->
+    <!--     <li class="hover:bg-blue-400 hover:rounded-full"> -->
+    <!--         <p class="m-10">{{item.text}}</p> -->
+    <!--     </li> -->
+    <!-- </ul> -->
   </div>
 </template>
 
@@ -24,10 +20,10 @@
 export default {
   data: () => ({
     items: [
-      { text: "Home", icon: "mdi-home-variant" },
-      { text: "Search", icon: "mdi-magnify" },
-      { text: "Bookmark", icon: "mdi-bookmark" },
-      { text: "Notification", icon: "mdi-bell" },
+      { text: "Home", icon: "mdi-home-variant", name: "index" },
+      { text: "Search", icon: "mdi-magnify", name: "search" },
+      { text: "Bookmark", icon: "mdi-bookmark", name: "bookmark" },
+      { text: "Notification", icon: "mdi-bell", name: "notification" },
     ],
   }),
 };
