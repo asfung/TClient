95899a829f6e3ba97a96cdee214f5560fce66d76
Paung <paungcuy15@gmail.com>
2025-04-06 14:17:06 +0700

    dasjds

--- pages/search.vue
+++ pages/search.vue
@@ -17,9 +17,10 @@
 
     <div class="result-post">
       <div v-if="searchFocus">
-        <div class="mx-4 my-2 space-y-4">
+        <!-- <div class="mx-4 my-2 space-y-4"> -->
+        <div class="my-2 space-y-4">
           <div class="flex items-center justify-between">
-            <div class="flex items-center">
+            <div class="flex items-center ml-3">
               <BeakerIcon class="size-5" />
               <span class="ml-2 text-sm font-semibold">Search for "{{ searchQuery }}"</span>
             </div>
@@ -27,7 +28,7 @@
           <v-divider></v-divider>
 
           <div v-if="userSearch.length">
-            <p class="font-bold">Users</p>
+            <p class="font-bold ml-3">Users</p>
             <v-list class="dark:bg-black dark:text-white">
               <v-list-item
                 v-for="item in userSearch"
@@ -36,19 +37,28 @@
                 :ripple="false"
                 @mousedown.prevent
                 @click="clickUser(item)"
-              >
+                height="60"
+                >
                 <template v-slot:title>
                   <div :class="$chaosOrb(item.username)" v-html="item.display_name"></div>
                 </template>
                 <template v-slot:subtitle>
                   <div v-html="item.username"></div>
                 </template>
+                <template v-slot:append>
+                  <button 
+                    @click.stop="toggleFollowFetch(item)"
+                    :class="item?.followed ? 'ring-1 ring-primaryLight dark:ring-primaryDark dark:text-white' : ' bg-primaryLight dark:bg-primaryDark text-white'"
+                    class="font-bold py-2 px-4 rounded-full">
+                    {{ item?.followed ? 'Followed' : 'Follow' }}
+                  </button>
+                </template>
               </v-list-item>
             </v-list>
           </div>
 
           <div v-if="postsSearch.length">
-            <p class="font-bold mt-4">Posts</p>
+            <p class="font-bold mt-4 ml-3">Posts</p>
             <div v-for="(item, index) in postsSearch" :key="index">
               <PostItem :item="item" :index="index" />
             </div>
@@ -114,6 +124,20 @@ const postSearchFetch = debounce(async () => {
   }
 }, 300)
 
+const toggleFollowFetch = async (user) => {
+  const fetch = await userStore.toggleFollow({
+    user_id_followed: user.id
+  })
+
+  user.followed = fetch.state
+
+  if (fetch.state) {
+    user.followers_count = (user.followers_count || 0) + 1
+  } else {
+    user.followers_count = (user.followers_count || 1) - 1
+  }
+}
+
 watch(searchQuery, (newQuery) => {
   userSearchFetch()
   postSearchFetch()
@@ -143,7 +167,8 @@ const submitSearch = () => {
 }
 
 const clickUser = (item) => {
-  console.log("Selected user:", item)
+  console.log("selected user:", item)
+  useNuxtApp().$router.push(`/@${item.username}`)
   searchFocus.value = true
 }
 
