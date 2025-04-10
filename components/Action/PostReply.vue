586010833285a89fd909082e3ae2268a24041097
Paung <paungcuy15@gmail.com>
2025-04-10 21:27:51 +0700

    reply comeny

--- components/Action/PostReply.vue
+++ components/Action/PostReply.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex items-center py-1">
   <button
+    @click.stop="replyDialog = !replyDialog"
     class="group flex items-center text-gray-500 px-3 py-2 text-base leading-6 font-medium rounded-full hover:bg-primaryLight hover:bg-opacity-10 hover:text-primaryLight transition-transform duration-200 ease-in-out active:scale-90">
     <svg class="h-7 w-7" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
       stroke="currentColor" viewBox="0 0 24 24">
@@ -12,14 +13,27 @@
       <NumberCounter :count="props.count" />
     </div>
   </button>
+  <DialogTextArea :dialog="replyDialog" @close-dialog="closeReplyDialog" :parent_id="props.parent_id" @post-created="handlePostCreated" />
   </div>
 </template>
 
 <script setup>
+const replyDialog = ref(false)
 const props = defineProps({
   count: {
     required: true,
+  },
+  parent_id: {
+    required: true
   }
 })
+
+const closeReplyDialog = () => {
+  replyDialog.value = false
+}
+
+const handlePostCreated = (newPost) => {
+  console.log(newPost)
+}
 
 </script>
